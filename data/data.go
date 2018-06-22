d2fb87809df83f02b361b5eeac1f100c72b3449c
ryanzola <ryanzola@me.com>
2018-06-22 09:46:03 -0400

    data and resolver functions

---
+++ data/data.go
@@ -0,0 +1,68 @@
+package data
+
+import (
+	"fmt"
+
+	"github.com/ryanzola/GoLangGraphQL/models"
+)
+
+// Books object model
+var Books = []*models.Book{}
+
+// Authors object model
+var Authors = []*models.Author{}
+
+func init() {
+	book1 := &models.Book{
+		ID:       "5b25981017052433f920f119",
+		Name:     "Catcher in the Rye",
+		Genre:    "Fiction",
+		AuthorID: "5b25935f44b43333c8da8707",
+	}
+	book2 := &models.Book{
+		ID:       "5b25cbd549a7cc37f48b91df",
+		Name:     "Harry Potter and the Sorcerer's Stone",
+		Genre:    "Fantasy",
+		AuthorID: "5b25cad549a7cc37f48b91de",
+	}
+	author1 := &models.Author{
+		ID:   "5b25935f44b43333c8da8707",
+		Name: "J.D. Salinger",
+		Age:  94,
+	}
+	author2 := &models.Author{
+		ID:   "5b25cad549a7cc37f48b91de",
+		Name: "J.K. Rowling",
+		Age:  50,
+	}
+
+	Books = []*models.Book{
+		book1,
+		book2,
+	}
+
+	Authors = []*models.Author{
+		author1,
+		author2,
+	}
+}
+
+//GetBook retrieves a book for a given user ID
+func GetBook(id string) (*models.Book, error) {
+	for _, book := range Books {
+		if book.ID == id {
+			return book, nil
+		}
+	}
+	return nil, fmt.Errorf("Book (id: %v) was not found", id)
+}
+
+//GetAuthor retrieves an author for a given user ID
+func GetAuthor(id string) (*models.Author, error) {
+	for _, author := range Authors {
+		if author.ID == id {
+			return author, nil
+		}
+	}
+	return nil, fmt.Errorf("Author (id: %v) was not found", id)
+}
--- schema/schema.go
+++ schema/schema.go
@@ -2,56 +2,50 @@ package schema
 
 import (
 	"github.com/graphql-go/graphql"
-	"github.com/ryanzola/graphQLGo/server/models"
+	"github.com/ryanzola/GoLangGraphQL/models"
 )
 
 var Root graphql.Schema
 var BookType graphql.Object
 var AuthorType graphql.Object
 
-func init () {
-	BookType = graphql.NewObject(graphql.ObjectConfig {
-		Name: "book",
+func init() {
+	BookType = graphql.NewObject(graphql.ObjectConfig{
+		Name:        "book",
 		Description: "Book object",
 		Fields: graphql.FieldsThunk(func() graphql.Fields {
-			return graphql.Fields {
+			return graphql.Fields{
 				"id": &graphql.Field{
-					Type: graphql.ID,
-					Description: "Book Id"
-				},
-				"name": &graphql.Field {
-					Type: graphql.String,
-					Description: "Book title"
-				},
-				"genre": &graphql.Field {
-					Type: graphql.String,
-					Description: "Book genre"
-				},
-				"author": &graphql.Field {
-					Type: AuthorType,
+					Type:        graphql.ID,
+					Description: "Book Id"},
+				"name": &graphql.Field{
+					Type:        graphql.String,
+					Description: "Book title"},
+				"genre": &graphql.Field{
+					Type:        graphql.String,
+					Description: "Book genre"},
+				"author": &graphql.Field{
+					Type:        AuthorType,
 					Description: "Author of the book",
 					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-						if book, ok := p.Source (*models.Book); ok {
+						if book, ok := p.Source.(*models.Book); ok {
 							// Retrieve the author of the book
 							return data.GetAuthor(author.ID)
 						}
-						return nil nil
-					}
-				}
+						return nil, nil
+					},
+				},
 			}
-		})
+		}),
 	})
 
-	AuthorType = graphql.NewObject(graphql.ObjectConfig {
-		Name: "author",
+	AuthorType = graphql.NewObject(graphql.ObjectConfig{
+		Name:        "author",
 		Description: "Author object",
-		Fields: graphql.FieldsThunk(func () graphql.Fields {
-			return graphql.Fields {
-				"id": &graphql.Field {
-					Type: graphql.ID,
-					Description: "Author Id"
-				}
+		Fields: graphql.FieldsThunk(func() graphql.Fields {
+			return graphql.Fields{
+				"id": &graphql.Field{Type: graphql.ID, Description: "Author Id"},
 			}
-		})
+		}),
 	})
 }
