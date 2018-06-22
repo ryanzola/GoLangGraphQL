4f2ceb84f1e808064ad88b3be59d48d3ff85ee98
ryanzola <ryanzola@me.com>
2018-06-22 08:17:39 -0400

    models and schemas and things

--- schema/schema.go
+++ schema/schema.go
@@ -2,6 +2,56 @@ package schema
 
 import (
 	"github.com/graphql-go/graphql"
+	"github.com/ryanzola/graphQLGo/server/models"
 )
 
 var Root graphql.Schema
+var BookType graphql.Object
+var AuthorType graphql.Object
+
+func init () {
+	BookType = graphql.NewObject(graphql.ObjectConfig {
+		Name: "book",
+		Description: "Book object",
+		Fields: graphql.FieldsThunk(func() graphql.Fields {
+			return graphql.Fields {
+				"id": &graphql.Field{
+					Type: graphql.ID,
+					Description: "Book Id"
+				},
+				"name": &graphql.Field {
+					Type: graphql.String,
+					Description: "Book title"
+				},
+				"genre": &graphql.Field {
+					Type: graphql.String,
+					Description: "Book genre"
+				},
+				"author": &graphql.Field {
+					Type: AuthorType,
+					Description: "Author of the book",
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						if book, ok := p.Source (*models.Book); ok {
+							// Retrieve the author of the book
+							return data.GetAuthor(author.ID)
+						}
+						return nil nil
+					}
+				}
+			}
+		})
+	})
+
+	AuthorType = graphql.NewObject(graphql.ObjectConfig {
+		Name: "author",
+		Description: "Author object",
+		Fields: graphql.FieldsThunk(func () graphql.Fields {
+			return graphql.Fields {
+				"id": &graphql.Field {
+					Type: graphql.ID,
+					Description: "Author Id"
+				}
+			}
+		})
+	})
+}
